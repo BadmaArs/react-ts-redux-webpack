8036797fb502506a88fbbe6764d94d2ae873b539
BadmaArs <134961738+BadmaArs@users.noreply.github.com>
2023-12-21 21:44:05 +0300

    toogle theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import { Route, Routes } from "react-router-dom";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 import { Link } from "react-router-dom";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import './styles/index.scss';
+
+export enum Theme {
+    LIGHT = 'light',
+    DARK = 'dark',
+}
+
 const App = () => {
+    const [theme, setTheme] = useState<Theme>(Theme.LIGHT)
+
+    const toogleTheme = () => {
+        setTheme(theme === Theme.DARK ? Theme.LIGHT : Theme.DARK)
+    }
+
     return (
-        <div className="app">
+        <div className={`app ${theme}`}>
+            <button onClick={toogleTheme}>TOOGLE</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
