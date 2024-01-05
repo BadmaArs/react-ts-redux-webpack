1404810cc2b8c0f4f4eeb7a0a33ad87c201906c6
BadmaArs <134961738+BadmaArs@users.noreply.github.com>
2024-01-05 13:38:22 +0300

    use classNames and useTheme

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,15 @@ import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 import './styles/index.scss';
 import { ThemeContext, Theme } from "./theme/ThemeContext";
+import { useTheme } from "./theme/useTheme";
+import { classNames } from "./helpers/classNames/classNames";
 
 
 
 const App = () => {
-    const {theme, setTheme} = useContext(ThemeContext)
-
-    const toogleTheme = () => {
-        setTheme(theme === Theme.DARK ? Theme.LIGHT : Theme.DARK)
-    }
+    const { theme, toogleTheme } = useTheme()
     return (
-        <div className={`app ${theme}`}>
+        <div className={`${classNames('app', {hovered: true, selected: true}, [theme, 'cls1', 'cls2', 'cls3'])}`}>
             <button onClick={toogleTheme}>TOOGLE</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
